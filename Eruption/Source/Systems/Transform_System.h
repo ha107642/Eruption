1f06ffd0de64c97dcb53b80934faceea8828df53
Henrik Alnestig <henrikalnestig@gmail.com>
2017-11-01 15:19:44 +0100

    Remove tons of redundant code from Transform_System.

--- Eruption/Source/Systems/Transform_System.h
+++ Eruption/Source/Systems/Transform_System.h
@@ -16,15 +16,9 @@ struct Transform {
 
 class Transform_System : public System<Transform> { //Needs a better name... meh
 protected:
-	std::vector<Component_Holder<Transform>> target_components;
-	float interp_time = -1.0f;
-
 	void update(Transform* const t, Entity entity, Time& time) override;
-	void interpolate(Transform * const current, Transform * const target, float amount);
 	void update(Time & time) override;
-	void set_state(System_State state) override;
 	void set_state(System_State_Delta<Transform>& delta) override;
-	void delete_component(Entity entity) override;
 public:
 	//System_State_Delta<Transform> get_delta_state(System_State * base);
 };
